771ffde80b3dc73f975bd75836e3bafeb5719e95
olucascdev <lucasdevproject@gmail.com>
2025-05-29 22:14:37 -0300

    :truck: Created group route for tasks(CRUD)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,6 +11,15 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/tasks', [TaskController::class,'index'])->name('tasks.index');
+    Route::post('/tasks', [TaskController::class,'store'])->name('tasks.store');
+    Route::put('/tasks/{task}', [TaskController::class,'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class,'destroy'])->name('tasks.destroy');
+});
+
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
