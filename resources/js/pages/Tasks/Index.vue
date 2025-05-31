db8ed8a9f4b09bd850d3428eb3d125cc32146e7f
olucascdev <lucasdevproject@gmail.com>
2025-05-30 21:22:37 -0300

    :lipstick: Enhance To-do List layout with new UI components

--- resources/js/pages/Tasks/Index.vue
+++ resources/js/pages/Tasks/Index.vue
@@ -2,6 +2,9 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Card, CardContent,  CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -16,8 +19,27 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="To-do List" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <h1>teste</h1>
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 items-center">
+            <Card class="flex w-sm">
+                <CardHeader>
+                    <CardTitle>Today</CardTitle>
+                    <div class="flex w-full max-w-sm items-center gap-1.5">
+                        <Input id="#" type="#" placeholder="New Task" />
+                        <Button type="submit">
+                            + New Task
+                        </Button>
+                    </div>
+                </CardHeader>
+            </Card>
+
+            <Card class="flex w-sm">
+                <CardHeader>
+                    <CardTitle>Task</CardTitle>
+                    <div class="flex w-full max-w-sm items-center gap-1.5">
+                    </div>
+                </CardHeader>
+            </Card>
+
         </div>
     </AppLayout>
 </template>
