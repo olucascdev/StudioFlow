58c485664688965cc17e9615c76028a3db468ae0
olucascdev <lucasdevproject@gmail.com>
2025-06-02 22:22:01 -0300

    :seedling: Created Factory for List

---
+++ database/factories/ListsFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Lists>
+ */
+class ListsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title'=> fake()->name(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
