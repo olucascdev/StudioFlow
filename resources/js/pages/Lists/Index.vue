c3245a6091ed4f9c825a232a8e6c6d570addfaf0
olucascdev <lucasdevproject@gmail.com>
2025-06-09 11:41:17 -0300

    :art: Updated form and modification in refresh to page

--- resources/js/pages/Lists/Index.vue
+++ resources/js/pages/Lists/Index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head ,usePage } from '@inertiajs/vue3';
+import { Head ,usePage , useForm} from '@inertiajs/vue3';
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import DialogFormCreate from '@/components/DialogFormCreate.vue';
+import { computed } from 'vue';
 
 
 
@@ -14,8 +15,19 @@ interface List {
     title: string;
 }
 
-const { props } = usePage<{ lists?: List[] }>();
-const lists = props.lists || [];
+const page = usePage<{ lists?: List[] }>();
+const lists = computed(() => page.props.lists || []);
+
+const form = useForm({
+    title: '',
+});
+
+const addList = () => {
+
+    form.post(route('lists.store'), {
+        onSuccess: () => form.reset('title'),
+    });
+};
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -29,20 +41,22 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="To-do List" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 items-center">
-            <Card class="flex w-sm">
-                <CardHeader>
-                    <CardTitle>To-do List</CardTitle>
-                    <div class="flex w-full max-w-sm items-center gap-1.5">
-                        <Input id="#" type="#" placeholder="New List" />
-                        <Button type="submit">
-                            + New List
-                        </Button>
-                    </div>
-                </CardHeader>
-            </Card>
+        <form @submit.prevent="addList">
+            <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 items-center">
+                <Card class="flex w-sm">
+                    <CardHeader>
+                        <CardTitle>To-do List</CardTitle>
+                        <div class="flex w-full max-w-sm items-center gap-1.5">
+                            <Input v-model="form.title" id="list-title" type="text" placeholder="New List" />
+                            <Button type="submit" :disabled="form.processing">
+                                + New List
+                            </Button>
+                        </div>
+                    </CardHeader>
+                </Card>
 
-        </div>
+            </div>
+        </form>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5 place-items-center">
                 <Card v-for="list in lists" :key="list.id" class="w-xs">
                         <CardHeader class="pb-3 ">
