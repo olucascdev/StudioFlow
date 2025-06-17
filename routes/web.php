06cec78663c1028e93e1a50f07c78e9bf961051a
olucascdev <lucasdevproject@gmail.com>
2025-06-17 20:00:00 -0300

    :truck: Add task management routes under specific lists in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -17,6 +18,17 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/lists', [ListController::class,'store'])->name('lists.store');
     Route::put('/lists/{lists}', [ListController::class,'update'])->name('lists.update');
     Route::delete('/lists/{lists}', [ListController::class,'destroy'])->name('lists.destroy');
+
+    //Route para exibit listas especificas com as tasks
+    Route::get('/lists/{lists}/tasks', [ListController::class,'show'])->name('lists.show');
+
+    Route::prefix('lists/{lists}')->name('lists.')->group(function () {
+        Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+        Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+        Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
+        Route::patch('/tasks/{task}/reorder', [TaskController::class, 'reorder'])->name('tasks.reorder');
+        Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+    });
 });
 
 
